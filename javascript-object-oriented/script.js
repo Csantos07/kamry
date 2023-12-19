4270d3f4e908485236779065aff957234d08f29e
Kamry Wilson <kamry55@gmail.com>
2023-12-19 15:57:55 -0500

    created my own function for class

--- javascript-object-oriented/script.js
+++ javascript-object-oriented/script.js
@@ -12,9 +12,14 @@ class Book {
     this.pageLength = pageLength;
     this.genre = genre;
   }
+
+  displayBookInformation() {
+    return `The title of this book is; ${this.title}, and the author is ${this.author}.`;
+  }
 }
 
 
 const favoriteBook = new Book("Candy House", "Jennifer Egan", 315, "fantasy fiction");
 
 console.log(favoriteBook);
+//console.log(favoriteBook.displayBookInformation());
